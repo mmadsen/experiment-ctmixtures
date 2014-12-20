451d9d85c5016f18d2c3fcf87b5869273f3be9c2
Mark Madsen <mark@madsenlab.org>
2014-12-19 16:21:09 -0800

    Working on paper, getting confusion matrices through xtable.

--- analysis/equifinality-4/paper-analysis.r
+++ analysis/equifinality-4/paper-analysis.r
@@ -1,7 +1,9 @@
-
+library(caret)
 library(mmadsenr)
+library(xtable)
+getOption('xtable.comment',FALSE)
 
-file_list <- c(
+results_files <- c(
   "balancedbias-neutral-comparison-gbm.RData",
   "bias-model-comparisons-gbm.RData",
   "classification-combined-tassize-combined_results-gbm.RData",
@@ -16,5 +18,74 @@ file_list <- c(
 for(file in results_files) {
   load(get_data_path(suffix = "experiment-ctmixtures/equifinality-4/results", filename = file))
 }
+
+model_objects <- c(
+  bias_results_model,
+  biasneutral_results_model,
+  biasneutral_tassize_model,
+  combined_tassize_results_model,
+  results_model,
+  tassize_perlocus_model,
+  tassize_subset_model,
+  tassize_biased_model,
+  perlocus_results_model
+)
+
+cm_objects <- c(
+  bias_results_cm,
+  biasneutral_results_cm,
+  biasneutral_tassize_cm,
+  combined_tassize_results_cm,
+  results_cm,
+  tassize_perlocus_cm,
+  tassize_subset_cm,
+  tassize_biased_cm,
+  perlocus_results_cm
+)
+
+
+## Capture Variable Importance ##
+
+var_importance_list <- NULL
+m_names <- names(model_objects)
+for(name in m_names) {
+  print(name)
+  var_importance_list[[name]] <- get_sorted_variable_importance(model_objects[[name]])
+}
+
+# save objects from the environment
+image_file <- get_data_path(suffix = "experiment-ctmixtures/equifinality-4/results", filename = "varimp-merged-gbm.RData")
+save(var_importance_list, file=image_file)
+
+
+## Formatted Confusion Matrices ##
+
+formatted_cm_list <- NULL
+m_names <- names(cm_objects)
+for(name in m_names) {
+  print(name)
+  formatted_cm_list[[name]] <- toLatex(xtable(cm_objects[[name]][["table"]],
+                                      align="|c|c|c|"), comment=FALSE,floating=FALSE)
+}
+
+# save objects from the environment
+image_file <- get_data_path(suffix = "experiment-ctmixtures/equifinality-4/results", filename = "cm-formatted-gbm.RData")
+save(formatted_cm_list, file=image_file)
+
+image_file <- get_data_path(suffix = "experiment-ctmixtures/equifinality-4/results", filename = "cm-merged-gbm.RData")
+save(cm_objects, file=image_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
