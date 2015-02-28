ff84c291de74a9759c1453a9876e11cb55b76802
Mark Madsen <mark@madsenlab.org>
2015-02-28 13:36:15 -0800

    Cleanup

--- analysis/equifinality-4/paperanalysis/results-create-variable-equifinality-example.r
+++ analysis/equifinality-4/paperanalysis/results-create-variable-equifinality-example.r
@@ -6,12 +6,13 @@ library(mvtnorm)
 library(ggplot2)
 library(ggthemes)
 library(mmadsenr)
+library(gridExtra)
 
 
 
 
 # Separable example
-p5 <- rmvnorm(500, c(101, 105))
+p5 <- rmvnorm(500, c(101, 103.5))
 p5_df <- as.data.frame(p5)
 p5_df$class <- "Model 1"
 
@@ -22,22 +23,40 @@ p6_df$class <- "Model 2"
 
 
 combined_3_df <- rbind(p5_df, p6_df)
-nonoverlapping <- ggplot(combined_3_df, aes(x = V1, y = V2)) + ggtitle("A")
+nonoverlapping <- ggplot(combined_3_df, aes(x = V1, y = V2)) 
 nonoverlapping <- nonoverlapping + geom_point(aes(color = class)) + xlab("Variable 1") + ylab("Variable 2") + theme_pander() 
 nonoverlapping <- nonoverlapping + guides(color = FALSE)
 nonoverlapping <- nonoverlapping + ylim(95,110) + xlim(95, 110)
 nonoverlapping
 
-variable1 <- ggplot(combined_3_df, aes(x = V1)) + ggtitle("B")
-variable1 <- variable1 + geom_density(aes(color = class), size = 2) + xlab("Density of Variable 1") + theme_pander() + guides(color = FALSE)
+variable1 <- ggplot(combined_3_df, aes(x = V1)) 
+variable1 <- variable1 + geom_density(aes(color = class), size = 1) + theme_pander() + guides(color = FALSE) 
+variable1 <- variable1 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
+                               axis.title.x = element_blank(), axis.title.y = element_blank(),
+                               panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 variable1
 
-variable2 <- ggplot(combined_3_df, aes(x = V2)) + ggtitle("C")
-variable2 <- variable2 + geom_density(aes(color = class), size = 2) + xlab("Density of Variable 2") + theme_pander() + guides(color = FALSE)
+variable2 <- ggplot(combined_3_df, aes(x = V2)) 
+variable2 <- variable2 + geom_density(aes(color = class), size = 1) + theme_pander() + guides(color = FALSE) 
+variable2 <- variable2 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
+                               axis.title.x = element_blank(), axis.title.y = element_blank(),
+                               panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+variable2 <- variable2 + coord_flip()
 variable2
 
 
+empty <- ggplot()+geom_point(aes(1,1), colour="white")+
+  theme(axis.ticks=element_blank(), 
+       panel.background=element_blank(), 
+       axis.text.x=element_blank(), axis.text.y=element_blank(),           
+       axis.title.x=element_blank(), axis.title.y=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-pdf(file = "../paper/figure/equifinality-variable-effect.pdf", width = 15, height = 10 )
-arrange_ggplot2(nonoverlapping, variable1, variable2)
+#grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
+
+
+
+pdf(file = "../paper/figure/generated/equifinality-variable-effect.pdf", width = 10, height = 10 )
+grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
+#arrange_ggplot2(nonoverlapping, variable1, variable2)
 dev.off()
+
