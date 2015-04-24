f13badd30d3645d96df9b8ae49f1c3b603754343
Mark Madsen <mark@madsenlab.org>
2015-04-23 18:17:11 -0700

    Much progress on revisions and getting now to the final results....

--- analysis/equifinality-4/paperanalysis/results-create-variable-equifinality-example.r
+++ analysis/equifinality-4/paperanalysis/results-create-variable-equifinality-example.r
@@ -7,12 +7,13 @@ library(ggplot2)
 library(ggthemes)
 library(mmadsenr)
 library(gridExtra)
+library(ggExtra)
 
 
 
 
 # Separable example
-p5 <- rmvnorm(500, c(101, 103.5))
+p5 <- rmvnorm(500, c(101, 105))
 p5_df <- as.data.frame(p5)
 p5_df$class <- "Model 1"
 
@@ -27,36 +28,40 @@ nonoverlapping <- ggplot(combined_3_df, aes(x = V1, y = V2))
 nonoverlapping <- nonoverlapping + geom_point(aes(color = class)) + xlab("Variable 1") + ylab("Variable 2") + theme_hc() + scale_colour_hc()
 nonoverlapping <- nonoverlapping + guides(color = FALSE)
 nonoverlapping <- nonoverlapping + ylim(95,110) + xlim(97, 105)
-
-
-variable1 <- ggplot(combined_3_df, aes(x = V1)) 
-variable1 <- variable1 + geom_density(aes(color = class), size = 1) + theme_hc() + scale_colour_hc()+ guides(color = FALSE) 
-variable1 <- variable1 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
-                               axis.title.x = element_blank(), axis.title.y = element_blank(),
-                               panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
-
-variable2 <- ggplot(combined_3_df, aes(x = V2)) 
-variable2 <- variable2 + geom_density(aes(color = class), size = 1) + theme_hc() + scale_colour_hc()+ guides(color = FALSE) 
-variable2 <- variable2 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
-                               axis.title.x = element_blank(), axis.title.y = element_blank(),
-                               panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-variable2 <- variable2 + coord_flip()
-
-
-
-empty <- ggplot()+geom_point(aes(1,1), colour="white")+
-  theme(axis.ticks=element_blank(), 
-       panel.background=element_blank(), 
-       axis.text.x=element_blank(), axis.text.y=element_blank(),           
-       axis.title.x=element_blank(), axis.title.y=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+ggMarginal(nonoverlapping, type="histogram")
 
 #grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
-
-
-grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
 pdf(file = "../paper/figure/generated/equifinality-variable-effect.pdf", width = 8, height = 8 )
-grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
+ggMarginal(nonoverlapping, type="histogram")
+#grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
 #arrange_ggplot2(nonoverlapping, variable1, variable2)
 dev.off()
+
+
+# variable1 <- ggplot(combined_3_df, aes(x = V1)) 
+# variable1 <- variable1 + geom_density(aes(color = class), size = 1) + theme_hc() + scale_colour_hc()+ guides(color = FALSE) 
+# variable1 <- variable1 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
+#                                axis.title.x = element_blank(), axis.title.y = element_blank(),
+#                                panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# 
+# 
+# variable2 <- ggplot(combined_3_df, aes(x = V2)) 
+# variable2 <- variable2 + geom_density(aes(color = class), size = 1) + theme_hc() + scale_colour_hc()+ guides(color = FALSE) 
+# variable2 <- variable2 + theme(axis.text.x= element_blank(), axis.text.y = element_blank(), 
+#                                axis.title.x = element_blank(), axis.title.y = element_blank(),
+#                                panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# variable2 <- variable2 + coord_flip()
+# 
+# 
+# 
+# empty <- ggplot()+geom_point(aes(1,1), colour="white")+
+#   theme(axis.ticks=element_blank(), 
+#        panel.background=element_blank(), 
+#        axis.text.x=element_blank(), axis.text.y=element_blank(),           
+#        axis.title.x=element_blank(), axis.title.y=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# 
+# #grid.arrange(variable1, empty, nonoverlapping, variable2, ncol=2,nrow=2,widths=c(4, 1), heights=c(1, 4))
+
+
+
 
