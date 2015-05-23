c2d8d517761e6ab8f8e060ab52e96c3df1d4ba9c
Mark Madsen <mark@madsenlab.org>
2015-05-23 09:37:54 -0700

    Non-tasampled analyses ready and tested.

--- analysis/equifinality-5/modelfitting/perlocus-analysis.r
+++ analysis/equifinality-5/modelfitting/perlocus-analysis.r
@@ -21,8 +21,29 @@ library(dplyr)
 library(ggthemes)
 
 
+# Overall timing
+ptm <- proc.time()
+
+########### Training and Tuning Variables ##############
+
+#
+# Common training and tuning parameters for ctmixtures analysis
+#
+
+gbm_grid <- expand.grid(interaction.depth = (1:6)*2,
+                        n.trees = (2:10)*50, 
+                        shrinkage = 0.05,
+                        n.minobsinnode = 10)
+
+training_control <- trainControl(method="repeatedcv", 
+                                 number=10, repeats=5)
 
 
+
+
+# make this repeatable - comment this out or change it to get a fresh analysis result
+seed_value <- 58132133
+set.seed(seed_value)
 
 log_file <- get_data_path(suffix = "experiment-ctmixtures/equifinality-5", filename = "perlocus-population-classification.log")
 flog.appender(appender.file(log_file), name='cl')
@@ -46,26 +67,7 @@ flog.info("Number of cores used in analysis: %s", num_cores, name='cl')
 registerDoMC(cores = num_cores)
 
 
-########### Training and Tuning Variables ##############
 
-#
-# Common training and tuning parameters for ctmixtures analysis
-#
-
-gbm_grid <- expand.grid(interaction.depth = (1:6)*2,
-                        n.trees = (2:10)*50, 
-                        shrinkage = 0.05,
-                        n.minobsinnode = 10)
-
-training_control <- trainControl(method="repeatedcv", 
-                                 number=10, repeats=5)
-
-
-
-
-# make this repeatable - comment this out or change it to get a fresh analysis result
-seed_value <- 58132133
-set.seed(seed_value)
 flog.info("RNG seed to replicate this analysis: %s", seed_value, name='cl')
 
 
@@ -85,9 +87,17 @@ perlocus_pop_cm <- NULL
 
 # prepare data
 
+## To Test, uncomment this
+# test_indices <- createDataPartition(eq5_pop_df$model_class_label, p = 0.005, list=FALSE)
+# eq5_pop_df <- eq5_pop_df[test_indices,]
+# flog.info("Test downsample to %.0f rows", nrow(eq5_pop_df), name='cl')
+
 eq5_neutral_conformist_df <- subset(eq5_pop_df, eq5_pop_df$model_class_label == "neutral" | eq5_pop_df$model_class_label == "conformist")
+eq5_neutral_conformist_df$two_class_label <- factor(eq5_neutral_conformist_df$model_class_label, levels=unique(eq5_neutral_conformist_df$model_class_label))
 eq5_neutral_anticonformist_df <- subset(eq5_pop_df, eq5_pop_df$model_class_label == "neutral" | eq5_pop_df$model_class_label == "anticonformist")
+eq5_neutral_anticonformist_df$two_class_label <- factor(eq5_neutral_anticonformist_df$model_class_label, levels=unique(eq5_neutral_anticonformist_df$model_class_label))
 eq5_pop_df$two_class_label <- factor(ifelse(eq5_pop_df$model_class_label == 'neutral', 'neutral', 'biased'))
+
 
 ############### Neutral vs Conformist Data #####################
 
@@ -98,11 +108,11 @@ i <- 1
 exp_name <- experiment_names[i]
 
 # remove fields from analysis that aren't predictors, and the detailed label with 3 classes
-exclude_columns <- c("simulation_run_id", "innovation_rate", "configuration_slatkin", "num_trait_configurations")
+exclude_columns <- c("simulation_run_id", "innovation_rate", "configuration_slatkin", "num_trait_configurations", "model_class_label")
 
 
 # fit GBM model
-model <- train_gbm_classifier(eq5_neutral_conformist_df, training_set_fraction, "model_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
+model <- train_gbm_classifier(eq5_neutral_conformist_df, training_set_fraction, "two_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
 
 
 perlocus_pop_model[["perlocus_census_conformist"]] <- model$tunedmodel
@@ -110,14 +120,14 @@ perlocus_pop_model[["perlocus_census_conformist"]] <- model$tunedmodel
 # use the test data split by the train_randomforest function and calculate tuned model predictions
 # and then get the confusion matrix and fitting metrics
 predictions <- predict(model$tunedmodel, newdata=model$test_data)
-cm <- confusionMatrix(predictions, model$test_data$model_class_label)
+cm <- confusionMatrix(predictions, model$test_data$two_class_label)
 results <- get_parsed_binary_confusion_matrix_stats(cm)
 results$experiments <- exp_name
 results$elapsed <- model$elapsed
 perlocus_pop_cm[["perlocus_census_conformist"]] <- cm
 
 # calculate a ROC curve
-population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "model_class_label", exp_name)
+population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "two_class_label", exp_name)
 results$auc <- unlist(population_roc$auc@y.values)
 perlocus_pop_results[["perlocus_census_conformist"]]  <- population_roc
 
@@ -130,11 +140,11 @@ i <- 2
 exp_name <- experiment_names[i]
 
 # remove fields from analysis that aren't predictors, and the detailed label with 3 classes
-exclude_columns <- c("simulation_run_id", "innovation_rate", "configuration_slatkin", "num_trait_configurations")
+exclude_columns <- c("simulation_run_id", "innovation_rate", "configuration_slatkin", "num_trait_configurations", "model_class_label")
 
 
 # fit GBM model
-model <- train_gbm_classifier(eq5_neutral_anticonformist_df, training_set_fraction, "model_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
+model <- train_gbm_classifier(eq5_neutral_anticonformist_df, training_set_fraction, "two_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
 
 
 perlocus_pop_model[["perlocus_census_anticonformist"]] <- model$tunedmodel
@@ -142,14 +152,14 @@ perlocus_pop_model[["perlocus_census_anticonformist"]] <- model$tunedmodel
 # use the test data split by the train_randomforest function and calculate tuned model predictions
 # and then get the confusion matrix and fitting metrics
 predictions <- predict(model$tunedmodel, newdata=model$test_data)
-cm <- confusionMatrix(predictions, model$test_data$model_class_label)
+cm <- confusionMatrix(predictions, model$test_data$two_class_label)
 results <- get_parsed_binary_confusion_matrix_stats(cm)
 results$experiments <- exp_name
 results$elapsed <- model$elapsed
 perlocus_pop_cm[["perlocus_census_anticonformist"]] <- cm
 
 # calculate a ROC curve
-population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "model_class_label", exp_name)
+population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "two_class_label", exp_name)
 results$auc <- unlist(population_roc$auc@y.values)
 perlocus_pop_results[["perlocus_census_anticonformist"]]  <- population_roc
 
@@ -221,7 +231,9 @@ save(perlocus_results_df, perlocus_pop_model, perlocus_pop_results, perlocus_pop
 flog.info("Saving just data frame of results of analysis to R environment snapshot: %s", image_file_results, name='cl')
 save(perlocus_results_df, file=image_file_results)
 
-# End
+
 flog.info("Analysis complete", name='cl')
+total_time <- proc.time() - ptm
+flog.info("Elapsed time in analysis: %.3f", total_time[3],name='cl')
 
 
--- analysis/equifinality-5/modelfitting/population-classification.r
+++ analysis/equifinality-5/modelfitting/population-classification.r
@@ -1,3 +1,19 @@
+## FILE: population-classification.r
+## EXPERIMENT:  equifinality-5
+## PROJECT:  experiment-ctmixtures
+## AUTHOR:  Mark E. Madsen
+## DATE:  5.20.15
+
+# Purpose:  To take simulated output from the CTMixtures simulation, which is time averaged and sampled,
+# and perform gradient boosted classification to measure our ability to distinguish between neutral,
+# conformist, and anticonformist cultural transmission given the availability of 26 predictor variables.
+# The models are fit using the Caret package to find the optimal model tuning (via 5 repeated of 10-fold CV),
+# and the final model assessed using a 20% test hold-out sample.  Test sample results are given by a
+# confusion matrix and ROC analysis, which are saved into a set of results objects and saved as binary
+# images to the filesystem for later use in paper preparation.
+# mmadsenr brings in GBM and other crucial analytic libraries
+
+
 library(randomForest)
 library(caret)
 library(doMC)
@@ -7,23 +23,23 @@ library(dplyr)
 library(ggthemes)
 
 
+# Overall timing
+ptm <- proc.time()
 
-# Train and tune random forest classifiers for each of the three data sets coming out of the experiment
-# "equifinality-5", for binary analysis. 
-#
-# Assumes that data-preparation.r has previously loaded CSV files and created binary 
-# data files, stored in a local data directory.  
-#
-# Reduces the four class data into a two class problem for basic classifier analysis, and it splits off
-# a test data set, placing it in the environment.  
-# 
-# NOTE:  This analysis takes a LONG time to run, so it is kept separate from the Rmarkdown
-# analysis script.  At the completion of the analysis, it saves an environment image, which
-# other scripts, such as RMarkdown documents, can load, and access the fitted model popsampled_results.  
-#
-# Only the model training and fitting is done in this script.  The test data set is not analyzed
-# here, since it can be done quickly enough that I want to be working in RMarkdown to examine different
-# options.  
+################# Training and Tuning Parameters #############
+## Caret training and tuning parameter grid
+gbm_grid <- expand.grid(interaction.depth = (1:6)*2,
+                        n.trees = (2:10)*50,
+                        shrinkage = 0.05,
+                        n.minobsinnode = 10)
+training_control <- trainControl(method="repeatedcv",
+                                 number=10, repeats=5)
+# make this repeatable - comment this out or change it to get a fresh analysis result
+seed_value <- 58132133
+set.seed(seed_value)
+# Set up sampling of train and test data sets
+training_set_fraction <- 0.8
+test_set_fraction <- 1.0 - training_set_fraction
 
 
 log_file <- get_data_path(suffix = "experiment-ctmixtures/equifinality-5", filename = "population-classification.log")
@@ -47,33 +63,7 @@ num_cores <- get_parallel_cores_given_os(dev=FALSE)
 flog.info("Number of cores used in analysis: %s", num_cores, name='cl')
 registerDoMC(cores = num_cores)
 
-
-########### Training and Tuning Variables ##############
-
-#
-# Common training and tuning parameters for ctmixtures analysis
-#
-
-gbm_grid <- expand.grid(interaction.depth = (1:6)*2,
-                        n.trees = (2:10)*50, 
-                        shrinkage = 0.05,
-                        n.minobsinnode = 10)
-
-training_control <- trainControl(method="repeatedcv", 
-                                 number=10, repeats=5)
-
-
-
-
-# make this repeatable - comment this out or change it to get a fresh analysis result
-seed_value <- 58132133
-set.seed(seed_value)
 flog.info("RNG seed to replicate this analysis: %s", seed_value, name='cl')
-
-
-# Set up sampling of train and test data sets
-training_set_fraction <- 0.8
-test_set_fraction <- 1.0 - training_set_fraction
 
 
 # set up popsampled_results data frames
@@ -87,8 +77,16 @@ pop_results_cm <- NULL
 
 # prepare data
 
+# To Test, uncomment this
+# test_indices <- createDataPartition(eq5_pop_df$model_class_label, p = 0.005, list=FALSE)
+# eq5_pop_df <- eq5_pop_df[test_indices,]
+# flog.info("Test downsample to %.0f rows", nrow(eq5_pop_df), name='cl')
+
+
 eq5_neutral_conformist_df <- subset(eq5_pop_df, eq5_pop_df$model_class_label == "neutral" | eq5_pop_df$model_class_label == "conformist")
+eq5_neutral_conformist_df$two_class_label <- factor(eq5_neutral_conformist_df$model_class_label, levels=unique(eq5_neutral_conformist_df$model_class_label))
 eq5_neutral_anticonformist_df <- subset(eq5_pop_df, eq5_pop_df$model_class_label == "neutral" | eq5_pop_df$model_class_label == "anticonformist")
+eq5_neutral_anticonformist_df$two_class_label <- factor(eq5_neutral_anticonformist_df$model_class_label, levels=unique(eq5_neutral_anticonformist_df$model_class_label))
 eq5_pop_df$two_class_label <- factor(ifelse(eq5_pop_df$model_class_label == 'neutral', 'neutral', 'biased'))
 
 ############### Neutral vs Conformist Data #####################
@@ -100,11 +98,11 @@ i <- 1
 exp_name <- experiment_names[i]
 
 # remove fields from analysis that aren't predictors, and the detailed label with 3 classes
-exclude_columns <- c("simulation_run_id", "innovation_rate")
+exclude_columns <- c("simulation_run_id", "innovation_rate", "model_class_label")
 
 
 # fit GBM model
-model <- train_gbm_classifier(eq5_neutral_conformist_df, training_set_fraction, "model_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
+model <- train_gbm_classifier(eq5_neutral_conformist_df, training_set_fraction, "two_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
 
 
 pop_results_model[["population_census_conformist"]] <- model$tunedmodel
@@ -112,14 +110,14 @@ pop_results_model[["population_census_conformist"]] <- model$tunedmodel
 # use the test data split by the train_randomforest function and calculate tuned model predictions
 # and then get the confusion matrix and fitting metrics
 predictions <- predict(model$tunedmodel, newdata=model$test_data)
-cm <- confusionMatrix(predictions, model$test_data$model_class_label)
+cm <- confusionMatrix(predictions, model$test_data$two_class_label)
 results <- get_parsed_binary_confusion_matrix_stats(cm)
 results$experiments <- exp_name
 results$elapsed <- model$elapsed
 pop_results_cm[["population_census_conformist"]] <- cm
 
 # calculate a ROC curve
-population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "model_class_label", exp_name)
+population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "two_class_label", exp_name)
 results$auc <- unlist(population_roc$auc@y.values)
 pop_results_roc[["population_census_conformist"]]  <- population_roc
 
@@ -132,11 +130,11 @@ i <- 2
 exp_name <- experiment_names[i]
 
 # remove fields from analysis that aren't predictors, and the detailed label with 3 classes
-exclude_columns <- c("simulation_run_id", "innovation_rate")
+exclude_columns <- c("simulation_run_id", "innovation_rate", "model_class_label")
 
 
 # fit GBM model
-model <- train_gbm_classifier(eq5_neutral_anticonformist_df, training_set_fraction, "model_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
+model <- train_gbm_classifier(eq5_neutral_anticonformist_df, training_set_fraction, "two_class_label", gbm_grid, training_control, exclude_columns, verbose=FALSE)
 
 
 pop_results_model[["population_census_anticonformist"]] <- model$tunedmodel
@@ -144,14 +142,14 @@ pop_results_model[["population_census_anticonformist"]] <- model$tunedmodel
 # use the test data split by the train_randomforest function and calculate tuned model predictions
 # and then get the confusion matrix and fitting metrics
 predictions <- predict(model$tunedmodel, newdata=model$test_data)
-cm <- confusionMatrix(predictions, model$test_data$model_class_label)
+cm <- confusionMatrix(predictions, model$test_data$two_class_label)
 results <- get_parsed_binary_confusion_matrix_stats(cm)
 results$experiments <- exp_name
 results$elapsed <- model$elapsed
 pop_results_cm[["population_census_anticonformist"]] <- cm
 
 # calculate a ROC curve
-population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "model_class_label", exp_name)
+population_roc <- calculate_roc_binary_classifier(model$tunedmodel, model$test_data, "two_class_label", exp_name)
 results$auc <- unlist(population_roc$auc@y.values)
 pop_results_roc[["population_census_anticonformist"]]  <- population_roc
 
@@ -223,7 +221,8 @@ save(pop_results, pop_results_model, pop_results_roc, pop_results_cm, file=image
 flog.info("Saving just data frame of results of analysis to R environment snapshot: %s", image_file_results, name='cl')
 save(pop_results, file=image_file_results)
 
-# End
 flog.info("Analysis complete", name='cl')
+total_time <- proc.time() - ptm
+flog.info("Elapsed time in analysis: %.3f", total_time[3],name='cl')
 
 
